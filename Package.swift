d9429fa21d3957bb5b8905637873815c25570116
Carmelo J Cortés Alhambra <>
2023-06-26 13:01:36 +0200

    Added xcframeworks info

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "MyLibraryHeadless",
-            targets: ["MyLibraryHeadless"]),
+            name: "MyLibraryCoreFramework",
+            targets: ["MyLibraryCoreFramework"]),
         .library(
             name: "MyFullLibrary2",
             targets: ["MyFullLibrary2"])
@@ -18,14 +18,14 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "MyLibraryHeadless",
-            url: "To replace",
-            checksum: "To replace"
+            name: "MyLibraryCoreFramework",
+            url: "https://github.com/carmelojcortes/MyLibrary/releases/download/0.0.7/MyLibCore.zip",
+            checksum: "8f0b32cdfd84b684db4c6de3bfe268d72ff80c6f9ed5d2bda2eae098cb85724c"
         ),
         .binaryTarget(
             name: "MyLibraryFramework",
-            url: "To replace",
-            checksum: "To replace"
+            url: "https://github.com/carmelojcortes/MyLibrary/releases/download/0.0.7/MyLib.zip",
+            checksum: "9a58451565591a031d99952040448cd709d5a94397b7eb3f5d08763fc34fa231"
         ),
         .target(
             name: "MyFullLibrary2",
